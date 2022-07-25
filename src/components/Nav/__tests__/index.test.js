c310640a5c81940cefd5c1fb3dfac12fd772c029
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-07-24 22:35:34 -0500

    testing the contact component

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -5,9 +5,23 @@ import Nav from '..';
 
 afterEach(cleanup);
 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+  ]
+  const mockCurrentCategory = jest.fn();
+  const mockSetCurrentCategory = jest.fn();
+  const mockContactSelected = jest.fn();
+  const mockSetContactSelected = jest.fn();
+
 describe('Nav component', () => {
   it('renders', () => {
-    render(<Nav />);
+    render(<Nav 
+    categories={categories}
+    setCurrentCategory={mockSetCurrentCategory}
+    currentCategory={mockCurrentCategory}
+    contactSelected={mockContactSelected}
+    setContactSelected={mockSetContactSelected}
+    />);
   });
 
   it('matches snapshot', () => {
