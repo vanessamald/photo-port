bca30e51c8b6d1699af7e0b89d9d821a366cc1da
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-07-24 20:16:41 -0500

    testing gallery and photolist components, all tests passing

--- src/App.js
+++ src/App.js
@@ -1,42 +1,31 @@
 import React, { useState } from 'react';
-import About from './components/About';
 import Nav from './components/Nav';
+import About from './components/About';
 import Gallery from './components/Gallery';
 
 function App() {
-  const [ categories ] = useState([
-    { 
-    name: "commercial", 
-    description: "Photos of grocery stores, food trucks, and other commercial projects",
-  },
-  {
-    name: "portraits", 
-    description: "Portraits of people in my life",
-  },
-  {
-    name: "food",
-    description: "Delicious delicacies",
-  },
-  {
-    name: "landscape",
-    description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-  }
-]);
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]); 
 
-  const [ currentCategory, setCurrentCategory ] = useState(categories[0]);
-  
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
-    <div> 
+    <div>
       <Nav
-        categories = {categories}
-        setCurrentCategory = {setCurrentCategory}
-        currentCategory = {currentCategory}
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
       ></Nav>
-       <main>
-        <div>
-        <Gallery></Gallery>
+      <main>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
-        </div>
       </main>
     </div>
   );
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from '../../assets/small/commercial/0.jpg';
+import PhotoList from '../PhotoList';
 
 function Gallery(props) {
-    const currentCategory = {
-        name: 'commercial',
-        description:
-            "Photos of grocery stores, food trucks, and other commercial projects",
-        };
+    const { currentCategory } = props;
+    //const { name, description } = currentCategory;
+
         return (
             <section>
-                <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-                <p>{currentCategory.name}</p>
-                <div className='flex-row'>
-                    <img src={photo} alt="Commercial Example" className="img-thumbnail mx-1" />
-                </div>
+                <h1 data-testid="h1tag">
+                    {capitalizeFirstLetter(currentCategory.name)}</h1>
+                <p>{currentCategory.description}</p>
+                <PhotoList category={currentCategory.name}></PhotoList>
             </section>
         );
     }
